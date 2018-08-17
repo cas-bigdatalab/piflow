947b606abd8ab558b7850650a8ec1723c2e2dd13
xiaoxiao <xiaoxiao@cnic.cn>
2018-08-17 11:24:30 +0800

    8.16

--- piflow-bundle/src/main/scala/cn/piflow/bundle/file/RegexTextProcess.scala
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/file/RegexTextProcess.scala
@@ -7,6 +7,7 @@ import cn.piflow.conf.{ConfigurableStop, FileGroup, StopGroup}
 import org.apache.spark.sql.SparkSession
 
 
+
 class RegexTextProcess extends ConfigurableStop{
     val inportCount: Int = 0
     val outportCount: Int = 1
@@ -16,12 +17,18 @@ class RegexTextProcess extends ConfigurableStop{
 
     def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
       val spark = pec.get[SparkSession]()
+      val sqlContext=spark.sqlContext
       val dfOld = in.read()
-      spark.udf.register("replace",(str:String)=>(str.replaceAll(regex,replaceStr)))
-      val dfNew=spark.sql("select *,replace(title) as title_new from dfOld")
+      val regexText=regex
+      val replaceText=replaceStr
+      dfOld.createOrReplaceTempView("thesis")
+      sqlContext.udf.register("regexPro",(str:String)=>str.replaceAll(regexText,replaceText))
+      val sqlText:String="select *,regexPro("+columnName+") as "+columnName+"_new from thesis"
+      val dfNew=sqlContext.sql(sqlText)
       dfNew.show()
       out.write(dfNew)
     }
+
 
   def initialize(ctx: ProcessContext): Unit = {
 
@@ -30,7 +37,7 @@ class RegexTextProcess extends ConfigurableStop{
 
   def setProperties(map: Map[String, Any]): Unit = {
     regex=MapUtil.get(map,key="regex").asInstanceOf[String]
-    columnName=MapUtil.get(map,key="columnNum").asInstanceOf[String]
+    columnName=MapUtil.get(map,key="columnName").asInstanceOf[String]
     replaceStr=MapUtil.get(map,key="replaceStr").asInstanceOf[String]
 
   }
