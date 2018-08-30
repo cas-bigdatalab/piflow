c1b362bf02e052f2f12a4671677e1564cf4c5c3a
judy0131 <xiaojie.zhu@163.com>
2018-08-30 14:39:06 +0800

    modify akka version

--- piflow-api/src/main/scala/cn/piflow/api/HTTPService.scala
+++ piflow-api/src/main/scala/cn/piflow/api/HTTPService.scala
@@ -1,24 +1,33 @@
 package cn.piflow.api
 
+import java.io.File
+import java.util.concurrent.CompletionStage
+
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Directives
-import spray.json.DefaultJsonProtocol
-import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.server.Directives
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Framing}
+import akka.util.ByteString
 import cn.piflow.api.util.PropertyUtil
 import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.Future
 import scala.util.parsing.json.JSON
 import cn.piflow.Process
+import org.apache.http.util.EntityUtils
+import spray.json.DefaultJsonProtocol
 
 
 object HTTPService extends DefaultJsonProtocol with Directives with SprayJsonSupport{
   implicit val system = ActorSystem("HTTPService", ConfigFactory.load())
   implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+  implicit val jsonStreamingSupport : JsonEntityStreamingSupport = EntityStreamingSupport.json
   var processMap = Map[String, Process]()
 
   def toJson(entity: RequestEntity): Map[String, Any] = {
@@ -32,17 +41,33 @@ object HTTPService extends DefaultJsonProtocol with Directives with SprayJsonSup
   }
 
   def route(req: HttpRequest): Future[HttpResponse] = req match {
+
    case HttpRequest(GET, Uri.Path("/"), headers, entity, protocol) => {
       Future.successful(HttpResponse(entity = "Get OK!"))
     }
 
    case HttpRequest(POST, Uri.Path("/flow/start"), headers, entity, protocol) =>{
-      //val flowJson = toJson(entity)
+
+     /*entity.getDataBytes().runFold(ByteString.empty, ByteString.materializer).thenCompose(r => {
+       val jsonString = r.utf8String
+     })*/
+
      entity match {
        case HttpEntity.Strict(_, data) =>{
          val process = API.startFlow(data.utf8String)
          processMap += (process.pid() -> process)
          Future.successful(HttpResponse(entity = process.pid()))
+       }
+       case HttpEntity.Default(_,contentLength,source)=>{
+
+         //source.runFoldAsync(ByteString.empty,materializer)
+         //entity.dataBytes.runWith(FileIO.toPath(new File("/opt/flow.json").toPath))
+         //val temp = entity.dataBytes.runWith(Sink.head).map(_.utf8String)
+         //entity.toStrict(3000,materializer).whenComplete((strict, th) => {println(strict.getData.utf8String)})
+         val process = API.startFlow("")
+         processMap += (process.pid() -> process)
+         Future.successful(HttpResponse(entity = process.pid()))
+
        }
        case _ => Future.failed(new Exception("Can not start flow!"))
      }
