1825eb07ffdba9734c07787d0b48063dbefdee90
judy0131 <xiaojie.zhu@163.com>
2018-09-29 17:53:30 +0800

    support multi sparkContext to run more than one flow at the same time(but only in standalone mode, yarn still have problems)

--- piflow-server/src/main/scala/cn/piflow/api/API.scala
+++ piflow-server/src/main/scala/cn/piflow/api/API.scala
@@ -1,21 +1,23 @@
 package cn.piflow.api
 
+import java.util.concurrent.CountDownLatch
+
 import cn.piflow.Runner
 import cn.piflow.conf.bean.{FlowBean, PropertyDescriptor}
 import org.apache.spark.sql.SparkSession
 import cn.piflow.conf.util.{ClassUtil, OptionUtil}
 import cn.piflow.Process
-import cn.piflow.api.util.{PropertyUtil}
+import cn.piflow.api.util.PropertyUtil
 import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet, HttpPost}
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import org.apache.spark.launcher.SparkLauncher
+import org.apache.spark.launcher.{SparkAppHandle, SparkLauncher}
 
 import scala.util.parsing.json.JSON
 
 object API {
 
-  def startFlow(flowJson : String):(String,Process) = {
+  /*def startFlow(flowJson : String):(String,Process) = {
 
     val map = OptionUtil.getAny(JSON.parseFull(flowJson)).asInstanceOf[Map[String, Any]]
     println(map)
@@ -53,22 +55,57 @@ object API {
     new Thread( new WaitProcessTerminateRunnable(spark, process)).start()
     (applicationId,process)
 
-    /*val launcher = new SparkLauncher
-    launcher.setMaster(PropertyUtil.getPropertyValue("spark.master"))
-      .setAppName("test")
-      .setDeployMode(PropertyUtil.getPropertyValue("spark.deploy.mode"))
-      .setConf("spark.hadoop.yarn.resourcemanager.hostname", PropertyUtil.getPropertyValue("yarn.resourcemanager.hostname"))
-      .setConf("spark.hadoop.yarn.resourcemanager.address", PropertyUtil.getPropertyValue("yarn.resourcemanager.address")).setConf("spark.yarn.access.namenode", PropertyUtil.getPropertyValue("yarn.access.namenode"))
-      .setConf("spark.yarn.stagingDir", PropertyUtil.getPropertyValue("yarn.stagingDir"))
-      .setConf("spark.yarn.jars", PropertyUtil.getPropertyValue("yarn.jars"))
-      .setConf("spark.jars", PropertyUtil.getPropertyValue("piflow.bundle"))
-      .setConf("hive.metastore.uris", PropertyUtil.getPropertyValue("hive.metastore.uris"))
-      .setMainClass("lalla")
-      .addAppArgs(flowJson)*/
+  }*/
+  def startFlow(flowJson : String):(String,SparkAppHandle) = {
+    var appId:String = null
+    val countDownLatch = new CountDownLatch(1)
+    val launcher = new SparkLauncher
+    val handle =launcher.setMaster(PropertyUtil.getPropertyValue("spark.master"))
+      //.setMaster(PropertyUtil.getPropertyValue("spark.master"))
+      //.setDeployMode(PropertyUtil.getPropertyValue("spark.deploy.mode"))
+      .setAppResource(PropertyUtil.getPropertyValue("piflow.bundle"))
+      //.setVerbose(true)
+      .setConf("spark.driver.memory", "1g")
+      .setConf("spark.executor.memory", "1g")
+      .setConf("spark.cores.max", "1")
+      .setMainClass("cn.piflow.api.StartFlowMain")
+      .addAppArgs(flowJson)
+      .startApplication( new SparkAppHandle.Listener {
+        override def stateChanged(handle: SparkAppHandle): Unit = {
+          appId = handle.getAppId
+          val sparkAppState = handle.getState
+          if(appId != null){
+            println("Spark jon with app id: " + appId + ",\t State changed to: " + sparkAppState)
+          }else{
+            println("Spark jon's state changed to: " + sparkAppState)
+          }
+          if (handle.getState().isFinal){
+            countDownLatch.countDown()
+            println("Task is finished!")
+          }
+        }
+        override def infoChanged(handle: SparkAppHandle): Unit = {
+          //println("Info:" + handle.getState().toString)
+        }
+      })
+    while (appId == null){
+      Thread.sleep(1000)
+    }
+    //println("Task is executing, please wait...")
+    //countDownLatch.await()
+    //println("Task is finished!")
 
+    /*launcher.launch()
+    val sparkAppHandle : SparkAppHandle = launcher.startApplication()
+
+    while(sparkAppHandle.getState != SparkAppHandle.State.FINISHED){
+      Thread.sleep(10000)
+      println("ApplicationId = " + sparkAppHandle.getAppId + "---Current State = " + sparkAppHandle.getState)
+    }*/
+    (appId, handle)
   }
 
-  def stopFlow(process : Process): String = {
+  def stopFlow(process : SparkAppHandle): String = {
     process.stop()
     "ok"
   }
---
+++ piflow-server/src/main/scala/cn/piflow/api/StartFlowMain.scala
@@ -0,0 +1,39 @@
+package cn.piflow.api
+
+import cn.piflow.Runner
+import cn.piflow.api.util.PropertyUtil
+import cn.piflow.conf.bean.FlowBean
+import cn.piflow.conf.util.OptionUtil
+import org.apache.spark.sql.SparkSession
+
+import scala.util.parsing.json.JSON
+
+object StartFlowMain {
+
+  def main(args: Array[String]): Unit = {
+    val flowJson = args(0)
+    val map = OptionUtil.getAny(JSON.parseFull(flowJson)).asInstanceOf[Map[String, Any]]
+    println(map)
+
+    //create flow
+    val flowBean = FlowBean(map)
+    val flow = flowBean.constructFlow()
+
+    //execute flow
+    val spark = SparkSession.builder()
+      .appName(flowBean.name)
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val process = Runner.create()
+      .bind(classOf[SparkSession].getName, spark)
+      .bind("checkpoint.path", PropertyUtil.getPropertyValue("checkpoint.path"))
+      .start(flow);
+    val applicationId = spark.sparkContext.applicationId
+    process.awaitTermination();
+    spark.close();
+    /*new Thread( new WaitProcessTerminateRunnable(spark, process)).start()
+    (applicationId,process)*/
+  }
+
+}
