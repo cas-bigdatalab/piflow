7ba388f39646b4b2ad523c205dfe13a10c12a8b1
yanfqidong0604 <wuiwuduzun521>
2018-10-13 11:31:49 +0800

    solr yang qi dong

---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/csv/CsvStringParser.scala
@@ -0,0 +1,84 @@
+package cn.piflow.bundle.csv
+
+import cn.piflow.{JobContext, JobInputStream, JobOutputStream, ProcessContext}
+import cn.piflow.conf.{ConfigurableStop, StopGroupEnum}
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.{ImageUtil, MapUtil}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions.monotonically_increasing_id
+
+class CsvStringParser extends ConfigurableStop{
+  override val authorEmail: String = "yangqidong@cnic.cn"
+  override val inportCount: Int = 0
+  override val outportCount: Int = 1
+  override val description: String = ""
+
+
+  var Str:String=_
+  var delimiter: String = _
+  var schema: String = _
+
+  override def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+
+    val session: SparkSession = pec.get[SparkSession]
+    val context: SparkContext = session.sparkContext
+
+    val arrStr: Array[String] = Str.split("\n")
+
+    var num:Int=0
+    val listROW: List[Row] = arrStr.map(line => {
+      val seqSTR: Seq[String] = line.split(delimiter).toSeq
+      num=seqSTR.size
+      val row = Row.fromSeq(seqSTR)
+      row
+    }).toList
+    val rowRDD: RDD[Row] = context.makeRDD(listROW)
+
+    var Fdf: DataFrame =null
+    if(schema.length==0){
+      (0 until num).foreach(x=>{
+        schema+=("_"+x+",")
+      })
+    }
+    val fields: Array[StructField] = schema.split(",").map(d=>StructField(d.trim,StringType,nullable = true))
+    val NewSchema: StructType = StructType(fields)
+    Fdf = session.createDataFrame(rowRDD,NewSchema)
+
+    Fdf.show(10)
+    out.write(Fdf)
+  }
+
+
+  override def setProperties(map: Map[String, Any]): Unit = {
+    Str = MapUtil.get(map,"Str").asInstanceOf[String]
+    delimiter = MapUtil.get(map,"delimiter").asInstanceOf[String]
+    schema = MapUtil.get(map,"schema").asInstanceOf[String]
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+    val Str = new PropertyDescriptor().name("Str").displayName("Str").defaultValue("").required(true)
+    descriptor = Str :: descriptor
+    val delimiter = new PropertyDescriptor().name("delimiter").displayName("delimiter").description("The delimiter of the String").defaultValue("").required(true)
+    descriptor = delimiter :: descriptor
+    val schema = new PropertyDescriptor().name("schema").displayName("schema").description("The schema of the String,The delimiter is ,").defaultValue("").required(false)
+    descriptor = schema :: descriptor
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] = {
+    ImageUtil.getImage("./src/main/resources/selectHiveQL.jpg")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroupEnum.CsvGroup.toString)
+  }
+
+  override def initialize(ctx: ProcessContext): Unit = {
+
+  }
+
+}
---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/csv/FolderCsvParser.scala
@@ -0,0 +1,99 @@
+package cn.piflow.bundle.csv
+
+import cn.piflow.{JobContext, JobInputStream, JobOutputStream, ProcessContext}
+import cn.piflow.conf.{ConfigurableStop, StopGroupEnum}
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.{ImageUtil, MapUtil}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions.monotonically_increasing_id
+
+class FolderCsvParser extends ConfigurableStop{
+  override val authorEmail: String = "yangqidong@cnic.cn"
+  override val inportCount: Int = 0
+  override val outportCount: Int = 1
+  override val description: String = ""
+
+
+  var FolderPath:String=_
+  var header:Boolean=_
+  var delimiter: String = _
+  var schema: String = _
+
+  override def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+
+    println("kkkkkkkkkkkkkkkk")
+
+    val session: SparkSession = pec.get[SparkSession]
+    val context: SparkContext = session.sparkContext
+
+
+
+    val StrRDD: RDD[String] = context.textFile(FolderPath)
+    var num:Int=0
+    var s:String=delimiter
+
+    val rowRDD: RDD[Row] = StrRDD.map(line => {
+      val seqSTR: Seq[String] = line.split(s).toSeq
+      num=seqSTR.size
+      val row = Row.fromSeq(seqSTR)
+      row
+    })
+
+
+
+    val schameARR: Array[String] = schema.split(",")
+    val fields: Array[StructField] = schameARR.map(d=>StructField(d.trim,StringType,nullable = true))
+
+    val NewSchema: StructType = StructType(fields)
+    val frame: DataFrame = session.createDataFrame(rowRDD,NewSchema)
+
+    val Fdf = frame.filter(row=>{
+      var bool: Boolean =true
+      for(x<-(0 until num)){
+        bool=row.get(x).equals(schameARR(x))
+      }
+      !bool
+    }).toDF()
+
+    Fdf.show(10)
+    out.write(Fdf)
+
+  }
+
+
+  override def setProperties(map: Map[String, Any]): Unit = {
+    FolderPath = MapUtil.get(map,"csvPath").asInstanceOf[String]
+    header = MapUtil.get(map,"header").asInstanceOf[String].toBoolean
+    delimiter = MapUtil.get(map,"delimiter").asInstanceOf[String]
+    schema = MapUtil.get(map,"schema").asInstanceOf[String]
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+    val FolderPath = new PropertyDescriptor().name("FolderPath").displayName("FolderPath").description("The path of csv Folder").defaultValue("").required(true)
+    descriptor = FolderPath :: descriptor
+    val header = new PropertyDescriptor().name("header").displayName("header").description("Whether the csv file have header or not").defaultValue("").required(true)
+    descriptor = header :: descriptor
+    val delimiter = new PropertyDescriptor().name("delimiter").displayName("delimiter").description("The delimiter of csv file").defaultValue("").required(true)
+    descriptor = delimiter :: descriptor
+    val schema = new PropertyDescriptor().name("schema").displayName("schema").description("The schema of csv file,The delimiter is ,").defaultValue("").required(true)
+    descriptor = schema :: descriptor
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] = {
+    ImageUtil.getImage("./src/main/resources/selectHiveQL.jpg")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroupEnum.CsvGroup.toString)
+  }
+
+  override def initialize(ctx: ProcessContext): Unit = {
+
+  }
+
+}
