14f1da58f2b6f072c07f1dd92a71dd4f83a55519
yanfqidong0604 <wuiwuduzun521>
2019-01-16 11:13:33 +0800

    qidong yang

---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/common/Distinct.scala
@@ -0,0 +1,58 @@
+package cn.piflow.bundle.common
+
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.{ImageUtil, MapUtil}
+import cn.piflow.conf.{ConfigurableStop, PortEnum, StopGroup}
+import cn.piflow.{JobContext, JobInputStream, JobOutputStream, ProcessContext}
+import org.apache.spark.sql.DataFrame
+
+class Distinct extends ConfigurableStop{
+  override val authorEmail: String = "yangqidong@cnic.cn"
+  override val description: String = "Reduplicate data according to all fields or fields you specify"
+  override val inportList: List[String] =List(PortEnum.DefaultPort.toString)
+  override val outportList: List[String] = List(PortEnum.DefaultPort.toString)
+
+  var files:String=_
+
+  override def setProperties(map: Map[String, Any]): Unit = {
+    files = MapUtil.get(map,"files").asInstanceOf[String]
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+
+    val files = new PropertyDescriptor().name("files").displayName("files").description("To de-duplicate the field, you can fill in the field name, " +
+      "if there are more than one, please use, separate. You can also not fill in, we will be based on all fields as a condition to weight.").defaultValue("").required(false)
+    descriptor = files :: descriptor
+
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] = {
+    ImageUtil.getImage("common/distinct.png")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroup.CommonGroup.toString)
+  }
+
+
+  override def initialize(ctx: ProcessContext): Unit = {
+
+  }
+
+  override def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+    val inDf: DataFrame = in.read()
+    var outDf: DataFrame = null
+    if(files.length > 0){
+      val fileArr: Array[String] = files.split(",")
+      outDf = inDf.dropDuplicates(fileArr)
+    }else{
+      outDf = inDf.distinct()
+    }
+    println("###########################################################################")
+    outDf.show(20)
+    println("###########################################################################")
+    out.write(outDf)
+  }
+}
---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/common/Subtract.scala
@@ -0,0 +1,57 @@
+package cn.piflow.bundle.common
+
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.ImageUtil
+import cn.piflow.conf.{ConfigurableStop, PortEnum, StopGroup}
+import cn.piflow.{JobContext, JobInputStream, JobOutputStream, ProcessContext}
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+class Subtract extends ConfigurableStop{
+  override val authorEmail: String = "yangqidong@cnic.cn"
+  override val description: String = "Delete duplicates of the first and second tables from the first table"
+  override val inportList: List[String] =List(PortEnum.AnyPort.toString)
+  override val outportList: List[String] = List(PortEnum.DefaultPort.toString)
+
+  override def setProperties(map: Map[String, Any]): Unit = {
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] = {
+    ImageUtil.getImage("common/subtract.png")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroup.CommonGroup.toString)
+  }
+
+
+  override def initialize(ctx: ProcessContext): Unit = {
+
+  }
+
+  override def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+    val spark = pec.get[SparkSession]()
+
+    val dfs: Seq[DataFrame] = in.ports().map(in.read(_))
+    var df1: DataFrame = dfs(0)
+    var df2: DataFrame = dfs(1)
+
+    val rdd: JavaRDD[Row] = df1.toJavaRDD.subtract(df2.toJavaRDD)
+
+    val schema: StructType = df1.schema
+    val outDF: DataFrame = spark.createDataFrame(rdd,schema)
+
+    println("##############################################################")
+    outDF.show(20)
+    println("##############################################################")
+
+    out.write(outDF)
+  }
+}
