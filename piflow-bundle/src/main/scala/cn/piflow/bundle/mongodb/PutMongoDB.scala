912dae9fe2159e83a0874455a58ff443dc626799
yanfqidong0604 <18525746364@163.com>
2019-04-15 15:23:52 +0800

    Optimizing the reading and writing of MongoDB QiDong Yang

---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/mongodb/PutMongoDB.scala
@@ -0,0 +1,65 @@
+package cn.piflow.bundle.mongodb
+
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.{ImageUtil, MapUtil}
+import cn.piflow.conf.{ConfigurableStop, PortEnum, StopGroup}
+import cn.piflow.{JobContext, JobInputStream, JobOutputStream, ProcessContext}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
+
+class PutMongoDB extends ConfigurableStop{
+  override val authorEmail: String = "yangqidong@cnic.cn"
+  override val description: String = "Put data to mongodb"
+  val inportList: List[String] = List(PortEnum.DefaultPort.toString)
+  val outportList: List[String] = List(PortEnum.NonePort.toString)
+
+  var ip:String=_
+  var port:String=_
+  var dataBase:String=_
+  var collection:String=_
+
+  override def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+    val spark: SparkSession = pec.get[SparkSession]()
+    val df: DataFrame = in.read()
+
+    df.write.options(
+      Map("spark.mongodb.output.uri" -> ("mongodb://" + ip + ":" + port + "/" + dataBase + "." + collection))
+    )
+      .mode("append")
+      .format("com.mongodb.spark.sql")
+      .save()
+  }
+
+  override def setProperties(map: Map[String, Any]): Unit = {
+    ip = MapUtil.get(map,"ip").asInstanceOf[String]
+    port = MapUtil.get(map,"port").asInstanceOf[String]
+    dataBase = MapUtil.get(map,"dataBase").asInstanceOf[String]
+    collection = MapUtil.get(map,"collection").asInstanceOf[String]
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+
+    val ip=new PropertyDescriptor().name("ip").displayName("ip").description("IP address,for example:0.0.0.1").defaultValue("").required(true)
+    descriptor = ip :: descriptor
+    val port=new PropertyDescriptor().name("port").displayName("port").description("the port").defaultValue("").required(true)
+    descriptor = port :: descriptor
+    val dataBase=new PropertyDescriptor().name("dataBase").displayName("dataBase").description("data base").defaultValue("").required(true)
+    descriptor = dataBase :: descriptor
+    val collection=new PropertyDescriptor().name("collection").displayName("collection").description("collection").defaultValue("").required(true)
+    descriptor = collection :: descriptor
+
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] =  {
+    ImageUtil.getImage("icon/mongoDB/PutMongo.png")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroup.Mongodb.toString)
+  }
+
+  override def initialize(ctx: ProcessContext): Unit = { }
+
+}
