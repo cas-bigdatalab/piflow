8b88500c1e8dd810c40171f999ddd16ef9d71c1e
judy_0131 <xiaojie.zhu@163.com>
2019-07-29 15:11:25 +0800

    fix putHiveQL bug

--- piflow-bundle/src/main/scala/cn/piflow/bundle/hive/PutHiveQL.scala
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/hive/PutHiveQL.scala
@@ -16,7 +16,7 @@ class PutHiveQL extends ConfigurableStop {
 
   var database:String =_
 
-    var hiveQL_path:String =_
+    var hiveQL_Path:String =_
 
     def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
       val spark = pec.get[SparkSession]()
@@ -26,7 +26,7 @@ class PutHiveQL extends ConfigurableStop {
       import scala.io.Source
       sql(sqlText= "use "+database)
       //val useDBText = "use "+database + ";"
-      var sqlString:String=HdfsUtil.getLines(hiveQL_path)
+      var sqlString:String=HdfsUtil.getLines(hiveQL_Path)
       sqlString.split(";").foreach( s => {
         println("Sql is " + s)
         sql(s)
@@ -39,16 +39,16 @@ class PutHiveQL extends ConfigurableStop {
     }
 
     def setProperties(map : Map[String, Any]): Unit = {
-      hiveQL_path = MapUtil.get(map,"hiveQL_Path").asInstanceOf[String]
+      hiveQL_Path = MapUtil.get(map,"hiveQL_Path").asInstanceOf[String]
       database = MapUtil.get(map,"database").asInstanceOf[String]
     }
 
   override def getPropertyDescriptor(): List[PropertyDescriptor] = {
     var descriptor : List[PropertyDescriptor] = List()
-    val hiveQL_path = new PropertyDescriptor().name("hiveQL_Path").displayName("HiveQL_Path").description("The hdfs path of the hiveQL file").defaultValue("").required(true)
+    val hiveQL_Path = new PropertyDescriptor().name("hiveQL_Path").displayName("HiveQL_Path").description("The hdfs path of the hiveQL file").defaultValue("").required(true)
     val database=new PropertyDescriptor().name("database").displayName("DataBase").description("The database name which the hiveQL" +
       "will execute on").defaultValue("").required(true)
-    descriptor = hiveQL_path :: descriptor
+    descriptor = hiveQL_Path :: descriptor
     descriptor = database :: descriptor
     descriptor
   }
