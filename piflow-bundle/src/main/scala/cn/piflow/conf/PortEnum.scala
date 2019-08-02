f57d03d58a9227b603a5723ce2b4da0aeaaf6912
or <1506355667@qq.com>
2019-08-02 17:47:40 +0800

    add nsfc scopus stop

--- piflow-bundle/src/main/scala/cn/piflow/conf/PortEnum.scala
+++ piflow-bundle/src/main/scala/cn/piflow/conf/PortEnum.scala
@@ -5,4 +5,6 @@ object PortEnum {
   val AnyPort = "Any"
   val DefaultPort = "Default"
   val NonePort = "None"
+  val scopus_articlePort = "scopus_articlePort"
+  val scopus_article_api_response = "scopus_article_api_response"
 }
