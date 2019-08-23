eec88f9ccd3d9bc3b31fd8993f4b23d22079de52
SongDz <songdongze@cnic.cn>
2019-08-23 11:06:56 +0800

    DropField Stop Completed

---
+++ piflow-bundle/src/main/scala/cn/piflow/bundle/common/DropField.scala
@@ -0,0 +1,55 @@
+package cn.piflow.bundle.common
+
+import cn.piflow._
+import cn.piflow.conf._
+import cn.piflow.conf.bean.PropertyDescriptor
+import cn.piflow.conf.util.{ImageUtil, MapUtil}
+
+
+class DropField extends ConfigurableStop {
+
+  val authorEmail: String = "ygang@cnic.cn"
+  val description: String = "drop data field"
+  val inportList: List[String] = List(PortEnum.DefaultPort.toString)
+  val outportList: List[String] = List(PortEnum.DefaultPort.toString)
+
+  var schema:String = _
+
+  def perform(in: JobInputStream, out: JobOutputStream, pec: JobContext): Unit = {
+    var df = in.read()
+
+    val field = schema.split(",")
+    for( x <- 0 until field.size){
+      df = df.drop(field(x))
+    }
+
+    out.write(df)
+  }
+
+  def initialize(ctx: ProcessContext): Unit = {
+
+  }
+
+  def setProperties(map : Map[String, Any]): Unit = {
+    schema = MapUtil.get(map,"schema").asInstanceOf[String]
+  }
+
+  override def getPropertyDescriptor(): List[PropertyDescriptor] = {
+    var descriptor : List[PropertyDescriptor] = List()
+    val inports = new PropertyDescriptor().name("schema").displayName("schema").description("The Schema you want to drop,Multiple are separated by commas").defaultValue("").required(true)
+    descriptor = inports :: descriptor
+    descriptor
+  }
+
+  override def getIcon(): Array[Byte] = {
+    ImageUtil.getImage("icon/common/Subtract.png")
+  }
+
+  override def getGroup(): List[String] = {
+    List(StopGroup.CommonGroup.toString)
+  }
+
+}
+
+
+
