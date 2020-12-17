0a4204cbac97ab9be947e24b8c215ff003337fe2
judy0131 <xiaojie.zhu@163.com>
2020-12-16 21:30:23 -0500

    fix bug: custom stop do not take effect after replacing jar

--- piflow-bin/start.sh
+++ piflow-bin/start.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 function pstart()
 {
-	#java -cp piflow-server-0.9.jar:./classpath/piflow-external.jar cn.piflow.api.Main
-	#java -cp piflow-server-0.9.jar:./classpath/* cn.piflow.api.Main
-        mkdir -p ./logs
-        classpathDir="./classpath"
-	if [ ! -e ./classpath/*.jar ];
-	then
-		echo "No Customized Stop!"
-		nohup java -cp ./lib/piflow-server-0.9.jar cn.piflow.api.Main -Djava.awt.headless=true > ./logs/piflow.log 2>&1 &
-	else
-		echo "Load Customized Stop from classpath"
-		nohup java -cp ./lib/piflow-server-0.9.jar:./classpath/* cn.piflow.api.Main -Djava.awt.headless=true > ./logs/piflow.log 2>&1 &
-	fi
-			 
-	#nohup java -cp ./lib/piflow-server-0.9.jar cn.piflow.api.Main -Djava.awt.headless=true > ./logs/piflow.log 2>&1 &
+        nohup java -cp ./lib/piflow-server-0.9.jar cn.piflow.api.Main -Djava.awt.headless=true -Djava.library.path=/usr/local/lib64/python3.6/site-packages/jep/ > ./logs/piflow.log 2>&1 &
 }
 pstart
 echo "PiFlow is running! check log in logs/piflow.log"
