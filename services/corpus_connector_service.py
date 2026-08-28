88f69c13ba653bc88a33460f3b9a3d9955460ae5
jen7871 <15076337871@163.com>
2026-08-28 14:55:59 +0800

    Modify corpus mount info

--- services/corpus_connector_service.py
+++ services/corpus_connector_service.py
@@ -456,7 +456,7 @@ def build_rustfs_mount_info(
             f"export RCLONE_CONFIG_MOUNTTMP_ACCESS_KEY_ID='{access_key}'",
             f"export RCLONE_CONFIG_MOUNTTMP_SECRET_ACCESS_KEY='{secret_key}'",
             "export RCLONE_CONFIG_MOUNTTMP_ENDPOINT='http://10.0.85.201:9000'",
-            "export RCLONE_CONFIG_MOUNTTMP_REGION='us-east-1'",
+            "export RCLONE_CONFIG_MOUNTTMP_REGION='cn-east-1'",
             "",
             "# 创建本地目录",
             mkdir_command,
@@ -480,7 +480,7 @@ def build_rustfs_mount_info(
             "RCLONE_CONFIG_MOUNTTMP_ACCESS_KEY_ID": access_key,
             "RCLONE_CONFIG_MOUNTTMP_SECRET_ACCESS_KEY": secret_key,
             "RCLONE_CONFIG_MOUNTTMP_ENDPOINT": "http://10.0.85.201:9000",
-            "RCLONE_CONFIG_MOUNTTMP_REGION": "us-east-1",
+            "RCLONE_CONFIG_MOUNTTMP_REGION": "cn-east-1",
         },
         "commands": {
             "installRclone": install_command,
